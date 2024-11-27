762905ae353357767398f403eaad4a5345a75d89
Arnulfo Gonzalez Jr <agonzalezjr327@gmail.com>
2024-11-26 17:55:53 -0700

    redid error component

--- src/pages/errors/error404/Error.js
+++ src/pages/errors/error404/Error.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Header from "../../../components/header/Header";
 import Footer from "../../../components/footer/Footer";
 import TopButton from "../../../components/topButton/TopButton";
+import React, { Component } from "react";
+import Greeting from "../../../containers/greeting/Greeting";
+import Skills from "../../../containers/skills/Skills";
 import { Fade } from "react-reveal";
 import "./Error.css";
 import { Link } from "react-router-dom";
@@ -10,27 +13,10 @@ export default class Error extends Component {
   render() {
     const theme = this.props.theme;
     return (
-      <div className="error-main">
+      <div>
         <Header theme={this.props.theme} />
-        <div className="error-class">
-          <Fade bottom duration={2000} distance="40px">
-            <h1>Woops</h1>
-            <h1 className="error-404">404</h1>
-            <p>The requested page is unavailable at the moment!</p>
-            <Link
-              className="main-button"
-              to="/home"
-              style={{
-                color: theme.body,
-                backgroundColor: theme.text,
-                border: `solid 1px ${theme.text}`,
-                display: "inline-flex",
-              }}
-            >
-              Go Home
-            </Link>
-          </Fade>
-        </div>
+        <Greeting theme={this.props.theme} />
+        <Skills theme={this.props.theme} />
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
       </div>
