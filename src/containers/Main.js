df126925124a52e4af42bad2b5c2972975f89e13
Arnulfo Gonzalez Jr <agonzalezjr327@gmail.com>
2025-01-02 15:50:28 -0700

    redid routes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Education from "../pages/education/EducationComponent";
@@ -7,28 +7,20 @@ import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
 
-export default class Main extends Component {
-  render() {
-    const { theme } = this.props; // Destructure props for cleaner code
+const Main = ({ theme }) => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home theme={theme} />} />
+        <Route path="/experience" element={<Experience theme={theme} />} />
+        <Route path="/education" element={<Education theme={theme} />} />
+        <Route path="/opensource" element={<Opensource theme={theme} />} />
+        <Route path="/contact" element={<Contact theme={theme} />} />
+        <Route path="/projects" element={<Projects theme={theme} />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-    return (
-      <BrowserRouter basename="/">
-        <Routes>
-          {/* Redirect from root to home */}
-          <Route path="/" element={<Navigate to="/home" replace />} />
-
-          {/* Define application routes */}
-          <Route path="/home" element={<Home theme={theme} />} />
-          <Route path="/experience" element={<Experience theme={theme} />} />
-          <Route path="/education" element={<Education theme={theme} />} />
-          <Route path="/opensource" element={<Opensource theme={theme} />} />
-          <Route path="/contact" element={<Contact theme={theme} />} />
-          <Route path="/projects" element={<Projects theme={theme} />} />
-
-          {/* Catch-all route for invalid paths */}
-          <Route path="*" element={<Navigate to="/home" replace />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
-}
+export default Main;
