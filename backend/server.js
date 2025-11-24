deeb1886722748a1a47e9c4af9293ba7a9719ef9
Arnulfo Gonzalez Jr <agonzalezjr327@gmail.com>
2025-11-24 13:07:57 -0700

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const chatRouter = require("./api/chat"); // ensure path is correct
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/chat", chatRouter); // must be a router function
