355b33def36e0dd6b1bd5169239518ab4d5f2ab9
Arnulfo Gonzalez Jr <agonzalezjr327@gmail.com>
2025-11-24 11:54:38 -0700

    redid server js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const serverless = require("serverless-http");
 
-// dotenv.config();
-// const app = express();
-// app.use(express.json());
+dotenv.config();
+const app = express();
+app.use(express.json());
 
-// // Enable CORS
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || [
-//       "http://localhost:3000",
-//       "https://agonzalezjr327.github.io"
-//     ].includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   }
-// }));
+// Enable CORS
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || [
+      "http://localhost:3000",
+      "https://agonzalezjr327.github.io"
+    ].includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 
-// // Import your chat route
-// const chatRoute = require("./chat");
-// app.use("/chat", chatRoute);
+// Import your chat route
+const chatRoute = require("./chat");
+app.use("/chat", chatRoute);
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server running on port ${port}`));
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
